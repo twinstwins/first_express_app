cc1cf7a1e68ee194b67c90e1c2fe690d0f99a284
twinstwins <weto2kakokako@yahoo.co.jpPrimary weto2kakokako@yahoo.co.jpPrimary weto2kakokako@yahoo.co.jpPrimary weto2kakokako@yahoo.co.jp>
2020-04-25 16:25:41 +0900

    はじめてのルーティング追加

--- routes/users_cool.js
+++ routes/users_cool.js
@@ -2,12 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send("fsafja");
-});
-
 router.get('/cool', function(req, res, next) {
-  res.send("you are so cool!!!!");
+  res.send("fsafja");
 });
 
 module.exports = router;
